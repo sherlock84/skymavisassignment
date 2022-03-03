6b770e03eec4708808edd80b3756185b12a0f506
Nguyen Thanh Ha <sherlock.nth@gmail.com>
2022-03-03 10:47:39 +0700

    Calculate wallet total amount from native amount of all assets.

--- pages/my-wallet.tsx
+++ pages/my-wallet.tsx
@@ -12,23 +12,26 @@ const assets: WalletAsset[] = [
   { amount: 10000, currency: "YEN", nativeAmount: 2103317, nativeCurrency: "VND" },
 ];
 
-const MyWalletPage: NextPage = () => (
-  <div className="flex flex-col items-stretch h-full my-5 gap-5">
-    <div className="flex flex-row justify-between">
-      <div className="flex flex-row items-center gap-3 rounded-lg bg-[#f7f9fc] py-1.5 px-4">
-        <Image src="/RoninWalletIcon.svg" width="16px" height="16px" alt="Ronin Wallet icon" />
-        <h1 className="text-xs">Ronin Wallet</h1>
+const MyWalletPage: NextPage = () => {
+  const totalAmount = React.useMemo(() => assets.reduce((sum, asset) => sum + asset.nativeAmount, 0), [assets]);
+  return (
+    <div className="flex flex-col items-stretch h-full my-5 gap-5">
+      <div className="flex flex-row justify-between">
+        <div className="flex flex-row items-center gap-3 rounded-lg bg-[#f7f9fc] py-1.5 px-4">
+          <Image src="/RoninWalletIcon.svg" width="16px" height="16px" alt="Ronin Wallet icon" />
+          <h1 className="text-xs">Ronin Wallet</h1>
+        </div>
+        <IconButton iconSrc="/Person.svg" />
       </div>
-      <IconButton iconSrc="/Person.svg" />
+      <WalletOverview currency="VND" locale="en-US" totalAmount={totalAmount} walletNumber="7300 3777 3888 3334" />
+      <div className="flex flex-row justify-center items-center gap-6">
+        <ActionButton iconSrc="/Deposit.svg" label="Deposit" disabled />
+        <ActionButton iconSrc="/Send.svg" label="Send" />
+        <ActionButton iconSrc="/Swap.svg" label="Swap" disabled />
+      </div>
+      <AssetList className="grow" assets={assets} locale="en-US" />
     </div>
-    <WalletOverview currency="VND" locale="en-US" totalAmount={26681289} walletNumber="7300 3777 3888 3334" />
-    <div className="flex flex-row justify-center items-center gap-6">
-      <ActionButton iconSrc="/Deposit.svg" label="Deposit" disabled />
-      <ActionButton iconSrc="/Send.svg" label="Send" />
-      <ActionButton iconSrc="/Swap.svg" label="Swap" disabled />
-    </div>
-    <AssetList className="grow" assets={assets} locale="en-US" />
-  </div>
-);
+  );
+};
 
 export default MyWalletPage;
